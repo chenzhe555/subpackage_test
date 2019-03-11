e57431b5ca13a0cb748195b64dfe77b0e2ae8e98
chenzhe <chenzhe@meicai.cn>
2019-03-11 15:48:17 +0800

    init project

---
+++ basic.config.js
@@ -0,0 +1,21 @@
+const pathSep = require('path').sep; //通过node.js path模块获取路径分割符
+function createCustomModuleIdFactory() {
+    const projectRootPath = __dirname; //node.js内置变量，获取项目路径，为了后面去掉路径前缀
+    return (path) => {
+        let customName = '';
+        if (path.indexOf(projectRootPath) == 0) {
+            //去掉路径前缀，因为里面带了当前打包机器的信息，不能保证唯一
+            customName = path.substr(projectRootPath.length+1);
+        }
+        //将所有正斜杠换成 _ (考虑分隔符有\\和/的情况)
+        let regExp = (pathSep == '\\') ? new RegExp('\\\\', 'gm') : new RegExp(pathSep, 'gm');
+        customName = customName.replace(regExp, '_');
+        return customName;
+    }
+}
+
+module.exports = {
+    serializer: {
+        createModuleIdFactory: createCustomModuleIdFactory
+    }
+}
